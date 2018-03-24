fd248f2da807ac30904e7fbfe3cb89054dad70bf
Musaddiqur Rahman <mashrur749@gmail.com>
2018-03-24 10:35:26 +0000

    modal done

---
+++ js/main.js
@@ -0,0 +1,31 @@
+// Get modal element
+var modal = document.getElementById('modal-window');
+// Get open modal button
+var modalBtn = document.getElementById('modal-btn');
+// Get close button
+var closeBtn = document.getElementsByClassName('close')[0];
+
+// Listen for open click
+modalBtn.addEventListener('click', openModal);
+// Listen for close click
+closeBtn.addEventListener('click', closeModal);
+// Listen for outside click
+window.addEventListener('click', outsideClick);
+
+// Function to open modal
+function openModal(){
+  modal.style.display = 'block';
+  modal.style.opacity = 1;
+}
+
+// Function to close modal
+function closeModal(){
+  modal.style.display = 'none';
+}
+
+// Function to close modal if outside click
+function outsideClick(e){
+  if(e.target == modal){
+    modal.style.display = 'none';
+  }
+}
