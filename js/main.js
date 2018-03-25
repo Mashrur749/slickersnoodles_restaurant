29dcb87becdcfd1336d5a163ba65e8721df8ee65
Musaddiqur Rahman <mashrur749@gmail.com>
2018-03-25 15:25:24 +0000

    meal combo fadeToggle added

--- js/main.js
+++ js/main.js
@@ -1,31 +1,45 @@
-// Get modal element
+
 var modal = document.getElementById('modal-window');
-// Get open modal button
 var modalBtn = document.getElementById('modal-btn');
-// Get close button
 var closeBtn = document.getElementsByClassName('close')[0];
 
-// Listen for open click
-modalBtn.addEventListener('click', openModal);
-// Listen for close click
-closeBtn.addEventListener('click', closeModal);
-// Listen for outside click
-window.addEventListener('click', outsideClick);
+$("#modal-btn").on("click",openModal);
+$(".close").on("click",closeModal);
+$(window).on("click",outsideClick);
 
-// Function to open modal
+
 function openModal(){
   modal.style.display = 'block';
-  modal.style.opacity = 1;
 }
 
-// Function to close modal
 function closeModal(){
   modal.style.display = 'none';
 }
 
-// Function to close modal if outside click
+
 function outsideClick(e){
   if(e.target == modal){
     modal.style.display = 'none';
   }
-}
+} 
+
+
+  $("a").on('click', function(event) {
+
+    if (this.hash !== "") {
+      event.preventDefault();
+
+      var hash = this.hash;
+
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 500, function(){
+        window.location.hash = hash;
+      });
+    } 
+  });
+
+$('.box h2').click(function(e){
+	var a = $(this).parent().children('div');
+	a.fadeToggle(1500);
+})
